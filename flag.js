8872b919aa9fdf9af8f5039986295f9089e6a93d
boujida2002 <117743753+boujida2002@users.noreply.github.com>
2024-05-28 12:42:49 +0100

    Update flag.js

--- flag.js
+++ flag.js
@@ -13,7 +13,8 @@ setTimeout(showContent, 2000);
 document.addEventListener("DOMContentLoaded", function() {
     const backgroundElement = document.getElementById("image-background");
     const images = [
-        "url('flag/')",
+        "url('flag/1ea4e62a-6c2c-4022-b087-ef3a15b43c9b.jpg')",
+        "url('flag/05b0e606-763c-40e4-9822-9ea03340319c.jpg')",
         "url('flag/05b0e606-763c-40e4-9822-9ea03340319c.jpg')"
      
         // Ajoutez autant d'images que nécessaire
